4a1d0287a76a65bef9a88100dfafceb4d6b78ddd
Piotrek <kapczynskipiotr@outlook.com>
2021-09-17 20:22:42 +0200

    project upload

--- src/main/java/tests/TestBase.java
+++ src/main/java/tests/TestBase.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.support.PageFactory;
 public class TestBase {
     public static WebDriver driver;
 
-
+//test comit
     public void setup(){
         WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
         driver = new ChromeDriver();
